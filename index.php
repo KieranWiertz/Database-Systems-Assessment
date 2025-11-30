bd2316a98620274cbcc2b0a076fef83218789f03
KieranWiertz <kieranwiertz@icloud.com>
2025-11-30 14:28:12 +0000

    Establish Database Connection for Staff Login

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
-    <title>Staff Login</title>
+    <title>Four Seasons Hotel Staff</title>
   </head>
   <body>
+    <h1 class="title">
+      Four Seasons Hotels <br />
+      Staff Portal
+    </h1>
     <div class="loginBody">
       <div class="wrapperLogin">
-        <h1 class="title">Staff Login</h1>
+        <h1 class="title1">Staff Login</h1>
         <form action="includes/login.inc.php" method="post">
           <div class="inputBox">
             <input type="text" name="uid" placeholder="Staff ID" required />
@@ -21,5 +25,15 @@
         </form>
       </div>
     </div>
+    
+    <?php
+      if (isset($_GET["error"])) {
+        if ($_GET["error"] == "emptyinput") {
+          echo "<p>Make sure to fill in all fields</p>";
+        } else if ($_GET["error"] == "wronglogin") {
+          echo "<p>Incorrect login details!</p>";
+        }
+      }
+    ?>
   </body>
 </html>
