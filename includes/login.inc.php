25bbdc4ca71c28c38b626283fe613ca446614fb6
KieranWiertz <kieranwiertz@icloud.com>
2025-12-06 13:27:31 +0000

    More Cleanup

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,5 +1,40 @@
 <?php
 session_start();
+function emptyInputLogin($username, $pwd) {
+    return empty($username) || empty($pwd);
+}
+
+function loginUser($conn, $username, $pwd) {
+    $username = trim($username);
+    $pwd = trim($pwd);
+
+    $sql = "SELECT * FROM Staff WHERE StaffID = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        die("SQL statement preparation failed.");
+    }
+
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultData)) {
+        $pwdHashed = $row["PasswordHash"];
+
+        if (hash("sha256", $pwd) === $pwdHashed) {
+            session_start();
+            $_SESSION["StaffID"] = $row["StaffID"];
+            header("location: ../staffMain.php");
+            exit();
+        } else {
+            header("location: ../index.php?error=wronglogin");
+            exit();
+        }
+    } else {
+        header("location: ../index.php?error=wronglogin");
+        exit();
+    }
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
@@ -7,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pwd = $_POST["pwd"];
 
     require_once 'dbh.inc.php';
-    require_once 'functions.inc.php';
+
 
     if (emptyInputLogin($username, $pwd)) {
         header("location: ../index.php?error=emptyinput");
@@ -20,3 +55,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     header("location: ../index.php");
     exit();
 }
+
+
